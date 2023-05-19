e84ecf66f69bab2afed6ecbe5f536a70b223c77d
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-05-19 15:23:34 -0600

    Added JS and eventListener to the buton with Thank you page

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevents form submission
+
+  var emailInput = document.querySelector('input[name="email"]');
+  var email = emailInput.value.trim();
+
+  // Regular expression pattern for email validation
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+   if (emailPattern.test(email)) {
+     document.body.innerHTML = `<div style="display:flex; align-items:center;
+      justify-content:center;"><h1>Thank you for subscribing!</h1></div>`;     
+  } else {
+
+    alert('Invalid email address. Please enter a valid email.');
+    emailInput.focus();
+  }
+});
+
+
+
